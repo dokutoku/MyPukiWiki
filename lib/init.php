62a3cd2e6954cf98718302a6f98336bff3d002bd
dokutoku <3729541-dokutoku@users.noreply.gitlab.com>
2020-07-28 04:16:04 +0900

    Changed the link in the footer.

--- lib/init.php
+++ lib/init.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 
 // PukiWiki version / Copyright / Licence
 define('S_VERSION', '1.5.3');
-define('S_COPYRIGHT', '<strong>MyPukiWiki '.S_VERSION.'</strong> &copy; 2020 <a href="https://gitlab.com/dokutoku/mypukiwiki" rel="external,nofollow">MyPukiWiki Development Team</a>');
+define('S_COPYRIGHT', '<a href="https://gitlab.com/dokutoku/mypukiwiki" rel="external,nofollow">MyPukiWiki</a> developed by <a href="https://twitter.com/dokutoku3" rel="external,nofollow">dokutoku</a>');
 
 /////////////////////////////////////////////////
 // Session security options
